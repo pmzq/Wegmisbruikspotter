20116b0575b248fc1eae35594d4bd897e98afbba
ferdihorst@gmail.com <Onxw1234>
2017-03-12 20:50:32 +0100

    Finalized facebook integration.

--- app/src/main/java/nl/wegmisbruikspotter/maps/MainLogin.java
+++ app/src/main/java/nl/wegmisbruikspotter/maps/MainLogin.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,9 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -26,6 +30,7 @@ public class MainLogin extends Activity {
     private TextView info;
     private LoginButton loginButton;
     private CallbackManager callbackManager;
+    private static final String TAG = "Test";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,45 +40,67 @@ public class MainLogin extends Activity {
         setContentView(R.layout.activity_main_login);
 
         FacebookSdk.sdkInitialize(getApplicationContext());
-        callbackManager = CallbackManager.Factory.create();
+        //callbackManager = CallbackManager.Factory.create();
 
         setContentView(R.layout.activity_main_login);
         info = (TextView)findViewById(R.id.info);
-        loginButton = (LoginButton)findViewById(R.id.login_button);
+        //loginButton = (LoginButton)findViewById(R.id.login_button);
 
-        FacebookSdk.sdkInitialize(getApplicationContext());
+        LoginManager.getInstance().logOut();
 
-        AccessToken accessToken = AccessToken.getCurrentAccessToken();
-        if (accessToken == null || accessToken.isExpired()) {
-            //login
-        } else {
-            ((Globals) getApplication()).setfacebookID(accessToken.toString());
+        Profile profile = Profile.getCurrentProfile();
+        if (profile != null) {
+            Log.v(TAG, "Logged, user name=" + profile.getFirstName() + " " + profile.getLastName());
+            ((Globals) getApplication()).setfacebookID(profile.getFirstName());
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
+        } else {
+            //login
         }
 
-
-
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("public_profile");
+        callbackManager = CallbackManager.Factory.create();
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+            private ProfileTracker mProfileTracker;
+
             @Override
             public void onSuccess(LoginResult loginResult) {
-                //info.setText("User ID: " + loginResult.getAccessToken().getUserId() + "\n" + "Auth Token: " + loginResult.getAccessToken().getToken());
-
-                Intent intent = new Intent(MainLogin.this, MainActivity.class);
-                ((Globals) getApplication()).setfacebookID(loginResult.getAccessToken().getUserId());
-                startActivity(intent);
-                finish();
-
+                if(Profile.getCurrentProfile() == null) {
+                    mProfileTracker = new ProfileTracker() {
+                        @Override
+                        protected void onCurrentProfileChanged(Profile profile, Profile profile2) {
+                            // profile2 is the new profile
+                            Log.v("facebook - profile", profile2.getFirstName());
+                            Intent intent = new Intent(MainLogin.this, MainActivity.class);
+                            Log.v(TAG, "Logged, user name=" + profile2.getFirstName());
+                            ((Globals) getApplication()).setfacebookID(profile2.getFirstName());
+                            startActivity(intent);
+                            mProfileTracker.stopTracking();
+                        }
+                    };
+                    // no need to call startTracking() on mProfileTracker
+                    // because it is called by its constructor, internally.
+                }
+                else {
+                    Profile profile = Profile.getCurrentProfile();
+                    Log.v("facebook - profile", profile.getFirstName());
+                    Log.v(TAG, "Logged, user name=" + profile.getFirstName());
+                    ((Globals) getApplication()).setfacebookID(profile.getFirstName());
+                    Intent intent = new Intent(MainLogin.this, MainActivity.class);
+                    startActivity(intent);
+                }
             }
 
             @Override
             public void onCancel() {
-                info.setText("Login attempt canceled.");
+                Log.v("facebook - onCancel", "cancelled");
             }
 
             @Override
             public void onError(FacebookException e) {
-                info.setText("Login attempt failed.");
+                Log.v("facebook - onError", e.getMessage());
             }
         });
     }
@@ -82,5 +109,6 @@ public class MainLogin extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
+
 
 }
